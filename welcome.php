e21b57c0d5083810eb26df055fe55193b6f877f4
83ctis38 <125125457+83ctis38@users.noreply.github.com>
2025-05-17 19:33:11 +0300

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -3,7 +3,7 @@ session_start();
 require "./db.php";
 
 
-if (!isset($_SESSION["user_id"]) || !isset($_SESSION["role'])) {
+if (!isset($_SESSION["user_id"]) || !isset($_SESSION["role"])) {
     if (isset($_COOKIE["remember_token"])) {
         $token = $_COOKIE["remember_token"];
 
